3bb530905c837327538449afa1f6f115434aeb5c
DanielMigchels <danielm12@hotmail.nl>
2020-09-04 11:36:35 +0200

    Remove ads from UI & improved UI

--- resources/views/products/layout.blade.php
+++ resources/views/products/layout.blade.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laravel 7 CRUD Application - ItSolutionStuff.com</title>
+    <title>Products</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
 </head>
 <body>
+
+<style>
+    .container {
+        margin-top: 50px;
+    }
+</style>
   
 <div class="container">
     @yield('content')
